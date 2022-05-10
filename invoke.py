75a021e3ca10cc6895b9ab61ea7fd44ef7041cd8
Steve Mattar <Steve.Mattar@kenshoo.com>
2022-05-11 02:08:05 +0300

    build: fix the package names
    Signed-off-by: Steve Mattar <Steve.Mattar@skai.io>

--- invoke.py
+++ invoke.py
@@ -1,11 +1,11 @@
 from lambda_tasks_plugin.lambda_module import LambdaModule
 
 stacks_to_lambda_module = {
-    'devops_bot_router': LambdaModule(packages=[], entry_point='devops-bot-jira-ticket/lambda_function.py'),
-    'devops_bot_jenkins': LambdaModule(packages=[], entry_point='devops-bot-jira-ticket/lambda_function.py'),
-    'devops_bot_jira_ticket': LambdaModule(packages=[], entry_point='devops-bot-jira-ticket/lambda_function.py'),
-    'devops_bot_lab_issue': LambdaModule(packages=[], entry_point='devops-bot-jira-ticket/lambda_function.py'),
-    'devops_bot_stackoverflow': LambdaModule(packages=[], entry_point='devops-bot-jira-ticket/lambda_function.py')
+    'devops_bot_router': LambdaModule(packages=[], entry_point='devops_bot_router/lambda_function.py'),
+    'devops_bot_jenkins': LambdaModule(packages=[], entry_point='devops_bot_jenkins/lambda_function.py'),
+    'devops_bot_jira_ticket': LambdaModule(packages=[], entry_point='devops_bot_jira_ticket/lambda_function.py'),
+    'devops_bot_lab_issue': LambdaModule(packages=[], entry_point='devops_bot_lab_issue/lambda_function.py'),
+    'devops_bot_stackoverflow': LambdaModule(packages=[], entry_point='devops_bot_stackoverflow/lambda_function.py')
 }
 
 repo_name = "devops-bot"
