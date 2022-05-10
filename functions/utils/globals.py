faacbe3c36564c42528ef09c40e62682f1781b30
Steve Mattar <Steve.Mattar@kenshoo.com>
2022-05-11 01:38:21 +0300

    test: fix
    Signed-off-by: Steve Mattar <Steve.Mattar@skai.io>

---
+++ functions/utils/globals.py
@@ -0,0 +1,5 @@
+from os import path
+
+
+APP_ROOT = path.dirname(path.dirname(__file__))
+PROJECT_NAME = path.basename(APP_ROOT)
--- tests/test_template.py
+++ tests/test_template.py
@@ -1,13 +1,13 @@
 from collections import Callable
 
-from functions.api_gateway_lambda import api_gateway_handler
+from functions.devops_bot_lab_issue import lambda_function as lab
 from functions.utils.globals import APP_ROOT, PROJECT_NAME
 
 
 class TestTemplate(object):
 
     def test_should_access_api_gateway_handler_modules_and_run_handler(self):
-        assert isinstance(api_gateway_handler.lambda_handler, Callable)
+        assert isinstance(lab.lambda_handler, Callable)
 
     def test_should_have_access_to_global_helper_objects(self):
-    	assert "devops-bot" in APP_ROOT
+        assert "devops-bot" in APP_ROOT
