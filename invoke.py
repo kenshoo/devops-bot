c5f1cca45203918ca5efe50153f1a4103f5594d3
Steve Mattar <Steve.Mattar@kenshoo.com>
2022-05-11 04:28:31 +0300

    build: add missing packages
    Signed-off-by: Steve Mattar <Steve.Mattar@skai.io>

--- invoke.py
+++ invoke.py
@@ -2,10 +2,10 @@ from lambda_tasks_plugin.lambda_module import LambdaModule
 
 stacks_to_lambda_module = {
     'devops_bot_router': LambdaModule(packages=[], entry_point='devops_bot_router/lambda_function.py'),
-    'devops_bot_jenkins': LambdaModule(packages=[], entry_point='devops_bot_jenkins/lambda_function.py'),
-    'devops_bot_jira_ticket': LambdaModule(packages=[], entry_point='devops_bot_jira_ticket/lambda_function.py'),
-    'devops_bot_lab_issue': LambdaModule(packages=[], entry_point='devops_bot_lab_issue/lambda_function.py'),
-    'devops_bot_stackoverflow': LambdaModule(packages=[], entry_point='devops_bot_stackoverflow/lambda_function.py')
+    'devops_bot_jenkins': LambdaModule(packages=['utils'], entry_point='devops_bot_jenkins/lambda_function.py'),
+    'devops_bot_jira_ticket': LambdaModule(packages=['utils'], entry_point='devops_bot_jira_ticket/lambda_function.py'),
+    'devops_bot_lab_issue': LambdaModule(packages=['utils'], entry_point='devops_bot_lab_issue/lambda_function.py'),
+    'devops_bot_stackoverflow': LambdaModule(packages=['utils'], entry_point='devops_bot_stackoverflow/lambda_function.py')
 }
 
 repo_name = "devops-bot"
