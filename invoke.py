d83cde12536fc5eadf64258fd0f7be636181a60e
Steve Mattar <Steve.Mattar@kenshoo.com>
2022-05-11 06:40:17 +0300

    fix: python packages paths
    Signed-off-by: Steve Mattar <Steve.Mattar@skai.io>

--- invoke.py
+++ invoke.py
@@ -2,11 +2,23 @@ from lambda_tasks_plugin.lambda_module import LambdaModule
 
 stacks_to_lambda_module = {
     'devops_bot_router': LambdaModule(packages=[], entry_point='devops_bot_router/lambda_function.py'),
-    'devops_bot_github_repo': LambdaModule(packages=['utils'], entry_point='devops_bot_github_repo/lambda_function.py'),
-    'devops_bot_jenkins': LambdaModule(packages=['utils'], entry_point='devops_bot_jenkins/lambda_function.py'),
-    'devops_bot_jira_ticket': LambdaModule(packages=['utils'], entry_point='devops_bot_jira_ticket/lambda_function.py'),
-    'devops_bot_lab_issue': LambdaModule(packages=['utils'], entry_point='devops_bot_lab_issue/lambda_function.py'),
-    'devops_bot_stackoverflow': LambdaModule(packages=['utils'], entry_point='devops_bot_stackoverflow/lambda_function.py')
+    'devops_bot_github_repo': LambdaModule(
+        packages=['utils'],
+        entry_point='devops_bot_github_repo/lambda_function.py'),
+    'devops_bot_jenkins': LambdaModule(
+        packages=[
+            'utils',
+            'devops_bot_jenkins'],
+        entry_point='devops_bot_jenkins/lambda_function.py'),
+    'devops_bot_jira_ticket': LambdaModule(
+        packages=['utils'],
+        entry_point='devops_bot_jira_ticket/lambda_function.py'),
+    'devops_bot_lab_issue': LambdaModule(
+        packages=['utils'],
+        entry_point='devops_bot_lab_issue/lambda_function.py'),
+    'devops_bot_stackoverflow': LambdaModule(
+        packages=['utils'],
+        entry_point='devops_bot_stackoverflow/lambda_function.py')
 }
 
 repo_name = "devops-bot"
